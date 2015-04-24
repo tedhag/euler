9899ae0a74562e176654cf313383dac47a03b76f
Thomas Edhag <thomas.edhag@smhi.se>
2015-04-24 16:01:00 +0200

    Cosmetic

--- haskell/src/uceuler1.hs
+++ haskell/src/uceuler1.hs
@@ -1,3 +1,3 @@
-mod35 = [x | x <-[1..999], (x `mod` 3==0) || (x `mod` 5==0)]
+multiples_of_3_and_5 = [x | x <-[1..999], (x `mod` 3==0) || (x `mod` 5==0)]
 
-solve id = "Solved UC "++show(id)++": Result is: "++show(sum mod35)
+solve id = "Solved UC "++show(id)++": Result is: "++show(sum multiples_of_3_and_5)
