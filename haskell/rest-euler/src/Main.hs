bf70e8d59b64c38c56b252b72a4d2d233793aa1f
Thomas Edhag <thomas.edhag@smhi.se>
2015-04-30 09:02:05 +0200

    Added a UC router

--- haskell/rest-euler/src/Main.hs
+++ haskell/rest-euler/src/Main.hs
@@ -2,12 +2,12 @@
 import Web.Scotty
 import Data.String
 
-import UCeuler1
+import RouteEulerUC
 
 main = scotty 3000 $ do
   get "/" $ do
     html "Hello! This is the root-URL for Haskell REST service"
   get "/uc/:id" $ do
     ucid <- param "id" :: ActionM Int
-    html $ (fromString (solve ucid))
+    html $ (fromString (route ucid))
     
---
+++ haskell/rest-euler/src/RouteEulerUC.hs
@@ -0,0 +1,9 @@
+module RouteEulerUC
+( route
+) where
+
+import UCeuler1
+
+route :: Int -> [Char]
+route 1 = solve 1
+route x = "UC not implemented"
