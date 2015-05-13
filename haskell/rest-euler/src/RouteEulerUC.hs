d041ae7846244cf64a5a3f3b2b4501d407e17a13
Thomas Edhag <thomas.edhag@smhi.se>
2015-05-13 12:16:05 +0200

    Added route for UC2

--- haskell/rest-euler/src/RouteEulerUC.hs
+++ haskell/rest-euler/src/RouteEulerUC.hs
@@ -2,8 +2,10 @@ module RouteEulerUC
 ( route
 ) where
 
-import UCeuler1
+import qualified UCeuler1 as UC1
+import qualified UCeuler2 as UC2
 
 route :: Int -> [Char]
-route 1 = solve 1
+route 1 = UC1.solve 1
+route 2 = UC2.solve 2
 route x = "UC not implemented"
