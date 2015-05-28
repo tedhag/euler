185922a2cd7104bfae76b862005d0052312e2fda
Thomas Edhag <thomas.edhag@smhi.se>
2015-05-28 15:16:33 +0200

    Added UC 3

--- haskell/rest-euler/src/RouteEulerUC.hs
+++ haskell/rest-euler/src/RouteEulerUC.hs
@@ -4,8 +4,10 @@ module RouteEulerUC
 
 import qualified UCeuler1 as UC1
 import qualified UCeuler2 as UC2
+import qualified UCeuler3 as UC3
 
 route :: Int -> [Char]
 route 1 = UC1.solve 1
 route 2 = UC2.solve 2
+route 3 = UC3.solve 3
 route x = "UC not implemented"
