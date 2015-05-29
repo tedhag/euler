1dffc51ff9b73cd55302fad658e3015645c55e81
Thomas Edhag <thomas.edhag@smhi.se>
2015-05-29 08:16:48 +0200

    Added some explanation comments

--- haskell/rest-euler/src/UCeuler3.hs
+++ haskell/rest-euler/src/UCeuler3.hs
@@ -4,14 +4,27 @@ module UCeuler3
 
 import Primes
 
--- 
+-- https://en.wikipedia.org/wiki/Prime_factor#Perfect_squares
+-- https://en.wikipedia.org/wiki/Square_number
+--
+-- fromIntegral: 
+-- For example, given an Int value n, one does not simply take 
+-- its square root by typing sqrt n, since sqrt can only be applied 
+-- to Floating-point numbers. Instead, one must write 
+-- sqrt (fromIntegral n) to explicitly convert n to a floating-point number.
+--
+-- '.' 
+-- In Haskell, '.' works like the UNIX |, but "backwards".
 perfect_squares n = takeWhile (< i) primes
-    where i = round . sqrt . fromIntegral $ n
+  where i = round . sqrt . fromIntegral $ n
 
+-- Anonymus function that filter if the reminder
+-- for n divided by n is zero, i.e. evenly divided by perfect_squares input
+-- (Perfect square numbers)
+prime_factors n = filter (\x -> ((n `mod` x) == 0)) (perfect_squares n)
 
-largestPrimeFactor :: Integer -> Integer
-largestPrimeFactor n =
-    maximum (filter (\x -> ((n `mod` x) == 0)) (perfect_squares n))
+largest_prime_factor :: Integer -> Integer
+largest_prime_factor n = maximum prime_factors n
 
 solve :: Int -> [Char]
-solve ucid = "Solved UC "++show(ucid)++": Result is: "++show(largestPrimeFactor 600851475143)
+solve ucid = "Solved UC "++show(ucid)++": Result is: "++show(largest_prime_factor 13195)
